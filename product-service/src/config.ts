22d54b38436ca52095715ef41d8b551185a2ecbd
jfct <jorgecosteixeira@gmail.com>
2024-07-29 18:45:27 +0100

    fix: corrected config URI for docker

--- product-service/src/config.ts
+++ product-service/src/config.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export const config = {
     mongodb: {
         // Use 'localhost' if debugging without docker
-        // uri: `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PWD}@mongodb:27017/${process.env.MONGODB_DB}?authSource=admin`
-        uri: `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PWD}@localhost:27017/${process.env.MONGODB_DB}?authSource=admin`
+        // uri: `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PWD}@localhost:27017/${process.env.MONGODB_DB}?authSource=admin`
+        uri: `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PWD}@mongodb:27017/${process.env.MONGODB_DB}?authSource=admin`
     },
 };
