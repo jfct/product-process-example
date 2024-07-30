149a73637cbed2ce304acc30e0c8a81b4f0fc312
jfct <jorgecosteixeira@gmail.com>
2024-07-30 21:47:25 +0100

    fix: reworked the dto models

--- product-service/src/dto/model.dto.ts
+++ product-service/src/dto/model.dto.ts
@@ -1,5 +1,20 @@
-import { IProduct } from "../models/product";
-import { IReview } from "../models/review";
+import { Schema } from "mongoose";
 
-export type CreateProductDto = Omit<IProduct, "reviewList" | "averageRating" | "deleted">
-export type CreateReviewDto = IReview;
+export interface ProductDto {
+    name: string;
+    description: string;
+    price: number;
+};
+export interface CreateProductDto extends ProductDto { };
+export interface UpdateProductDto extends Partial<CreateProductDto> { };
+
+
+export interface ReviewDto {
+    firstName: string;
+    lastName: string;
+    review: string;
+    rating: number;
+    productId: string | Schema.Types.ObjectId;
+};
+export interface CreateReviewDto extends ReviewDto { };
+export interface UpdateReviewDto extends Partial<Omit<CreateReviewDto, 'productId'>> { };
