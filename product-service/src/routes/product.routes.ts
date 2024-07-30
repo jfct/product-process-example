e00c8875df454156516d45c583d8e7ced105c556
jfct <jorgecosteixeira@gmail.com>
2024-07-30 21:49:54 +0100

    fix: changed the routers to new review logic

--- product-service/src/routes/product.routes.ts
+++ product-service/src/routes/product.routes.ts
@@ -1,15 +1,20 @@
 import { Router } from 'express';
 import ProductController from '../controller/product.controller';
-import { createProductValidation, idValidation, listValidation, updateProductValidation } from '../validations/product.validation';
+import { createProductValidation, updateProductValidation } from '../validations/product.validation';
+import { idValidation, listValidation } from '../validations/validation';
 
 const productRouter = Router();
 const productController = new ProductController();
 
 // Using .bind to maintain `this` context
 productRouter.get('/', listValidation, productController.listProducts.bind(productController));
-productRouter.post('/', createProductValidation, productController.createProduct.bind(productController));
-productRouter.get('/:id', idValidation, productController.getProductById.bind(productController));
-productRouter.put('/:id', [...idValidation, ...updateProductValidation], productController.updateProduct.bind(productController));
-productRouter.delete('/:id', idValidation, productController.deleteProduct.bind(productController));
+productRouter.get('/:id', idValidation, productController.get.bind(productController));
+productRouter.get('/reviews/:id', idValidation, productController.listReviewsForProduct.bind(productController));
+
+productRouter.post('/', createProductValidation, productController.create.bind(productController));
+
+productRouter.put('/:id', [...idValidation, ...updateProductValidation], productController.update.bind(productController));
+
+productRouter.delete('/:id', idValidation, productController.delete.bind(productController));
 
 export default productRouter;
